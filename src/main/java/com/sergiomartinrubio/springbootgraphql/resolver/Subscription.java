6309babc6264a47ba093e173e404236ac6a50635
Sergio Martin Rubio <econsergio@gmail.com>
2019-07-13 17:00:08 +0100

    remove publisher

--- src/main/java/com/sergiomartinrubio/springbootgraphql/resolver/Subscription.java
+++ src/main/java/com/sergiomartinrubio/springbootgraphql/resolver/Subscription.java
@@ -2,7 +2,6 @@ package com.sergiomartinrubio.springbootgraphql.resolver;
 
 import com.coxautodev.graphql.tools.GraphQLSubscriptionResolver;
 import com.sergiomartinrubio.springbootgraphql.model.Hotel;
-import com.sergiomartinrubio.springbootgraphql.publisher.HotelPublisher;
 import com.sergiomartinrubio.springbootgraphql.repository.HotelRepository;
 import lombok.RequiredArgsConstructor;
 import org.reactivestreams.Publisher;
